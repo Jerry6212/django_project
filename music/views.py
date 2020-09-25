176e2b3f269476a1b30c5057973c7b195d4b5efb
Jerry Ploegert <jerryjploegert@gmail.com>
2020-09-25 07:10:56 -0500

    Templates added
    This adds HTML templates, gives error when requested page does not exist, and makes the HTML dynamic.

--- music/views.py
+++ music/views.py
@@ -1,20 +1,12 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-from  django.http import Http404
-from django.http import HttpResponse
 from .models import Album
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     all_albums = Album.objects.all()
-    context = {
-        "all_albums": all_albums}
+    context = {"all_albums": all_albums}
     return render(request, "music/index.html", context)
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(id=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album not found")
-    return render(request, "music/index.html", context)
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, "music/detail.html", {"album": album})
